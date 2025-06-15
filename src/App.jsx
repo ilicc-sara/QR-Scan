acbefa4c271a30ad5d2020bce681fdcb81f982f2
Sara <sarailic431@gmail.com>
2025-06-15 17:39:02 +0200

    setting timer function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [time, setTime] = useState(0);
+
+  const timerFunction = function () {
+    let time = 11;
+
+    const tick = function () {
+      time--;
+
+      console.log(time);
+      let timeString = String(time);
+      setTime(timeString);
+
+      if (time === 0) {
+        clearInterval(timer);
+      }
+    };
+
+    tick();
+    const timer = setInterval(tick, 1000);
+  };
+
   return (
     <>
       <nav>
-        <button className="btn-active">Generate</button>
+        <button className="btn-active" onClick={() => timerFunction()}>
+          Generate
+        </button>
       </nav>
 
       <main>
@@ -14,6 +37,7 @@ function App() {
           <h4>Time is up!</h4>
           <button className="btn-active">Generate</button>
         </div>
+        <p>{time} s</p>
         <button className="download-btn btn-active">Download</button>
       </main>
     </>
